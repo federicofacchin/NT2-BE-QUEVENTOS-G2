382006e42a4067484f92d46bacaeb9d4331fe6fa
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2022-11-11 13:45:40 -0300

    Refactorizacion vistas login y validaciones

--- queventos/App.js
+++ queventos/App.js
@@ -1,17 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react'
 import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login'
+import SignUp from './screens/SignUp'
 import Notificactions from './screens/Notifications'
-
-
+import { Stack } from 'native-base';
 
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
+  const [authenticationData, setAuthenticationData] = useState(false)
+
   return (
-    <View style={styles.container}>
-      <Login></Login>
-      {/*<Notificactions></Notificactions>*/}
-    </View>
+
+    <NavigationContainer>
+      {
+        authenticationData ?
+        <Stack.Navigator>
+            <Stack.Screen
+              name="Notifications"
+              component={Notificactions}
+              options={
+                {
+                  title: "Notificaciones",
+                }
+              }/>
+        </Stack.Navigator>
+        :
+        <Stack.Navigator>
+              <Stack.Screen
+              name="Login"
+              component={Login}
+              options={
+                {
+                  headerShown: false
+                }
+              }/>
+            <Stack.Screen
+              name="SignUp"
+              component={SignUp}
+              options={
+                {
+                  headerShown: false
+                }
+              }/>
+        </Stack.Navigator>
+
+      }
+    </NavigationContainer>
   );
 }
 
--- queventos/helpers/index.js
+++ queventos/helpers/index.js
@@ -1,10 +1,17 @@
 
 
 const emailValidator = (email)=>{
-    const regex = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+    const regex = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     
     return regex.test(email)
 }
 
+const lengthValidator = (string, requiredLength)=>{
+    return string.length >= requiredLength
+}
 
-export { emailValidator };
+
+export default {
+    emailValidator,
+    lengthValidator,
+};
