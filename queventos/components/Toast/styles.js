cd5c807ae0d4142f314f8f9bad66b56352f64f05
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2022-11-27 15:39:21 -0300

    Se agrega retorno con ruta parametrizada al modificar el estado de una suscripcion

---
+++ queventos/components/Toast/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: "row",
+      alignItems: "center",
+      backgroundColor: "#111827",
+      position: "absolute",
+      left: 16,
+      right: 16,
+      bottom: 16,
+      padding: 16,
+      paddingLeft: 16,
+      paddingTop: 16,
+      paddingRight: 16,
+      paddingBottom: 16,
+      marginBottom: 8,
+      borderRadius: 8,
+      zIndex: 1100,
+    },
+    body: {
+      color: "white"
+    }
+  });
+
+export default styles
