e3dc88cfdbcb66c8c7181bb44be3780e878edf14
sistemas.nicolas <nwainschenker@prismamp.com>
2022-11-27 18:12:22 -0300

    fix/firebase_AsyncStorage

--- queventos/services/firebase.js
+++ queventos/services/firebase.js
@@ -1,8 +1,8 @@
 import Constants from 'expo-constants';
-
 import { initializeApp } from 'firebase/app'
-import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
+import { initializeAuth, getReactNativePersistence } from "firebase/auth/react-native"
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Inicializa la conexión firebase con nuestro proyecto
 export const firebaseApp = initializeApp({
@@ -12,20 +12,12 @@ export const firebaseApp = initializeApp({
     storageBucket: Constants.manifest.extra.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: Constants.manifest.extra.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: Constants.manifest.extra.REACT_APP_FIREBASE_APP_ID
-    //apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    //authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    //projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    //storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    //messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    //appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
 
-
-// Exporta esta constante para usarla en la vista del login
-const auth = getAuth(firebaseApp)
-
-// Exporta esta referencia para pedir los datos a la persistencia
 const db = getFirestore(firebaseApp)
+const auth = initializeAuth(firebaseApp, {
+    persistence: getReactNativePersistence(AsyncStorage)
+  })
 
 export {
     auth, db
